399c35f7acffbd15b75e6cd315a3cd1f3d5fa439
Mani <mgkums@gmail.com>
2022-10-01 23:39:21 +0600

    v3

--- server/routes/commonUsers.js
+++ server/routes/commonUsers.js
@@ -106,6 +106,46 @@ commonUsers.post("/comment-endorsement", async (req, res) => {
     }
   });
 
+  commonUsers.get("/get-endorsement", async (req, res) => {
+    const id = req.query.endorsementId;
+  
+    try {
+        const endorsement = await Endorsement.findById(id)
+        return res.status(200).json(endorsement);
+     
+    } catch (err) {
+      res.status(500).json("could not get specified endorsment!");
+    }
+  });
+
+  commonUsers.get("/get-endorsements-of-user", async (req, res) => {
+    const id = req.query.userId;
+  
+    try {
+        const endorsements = await Endorsement.find({"user.id":id})
+        return res.status(200).json(endorsements);
+     
+    } catch (err) {
+      res.status(500).json("could not get endorsments!");
+    }
+  });
+
+  commonUsers.get("/get-endorsements", async (req, res) => {
+    const {skip,limit} = req.query;
+    try {
+        const endorsements = await Endorsement.find({},
+          'title description image user comments',
+          {skip,limit})
+        return res.status(200).json(endorsements);
+     
+    } catch (err) {
+      console.log(err)
+      res.status(500).json("could not get endorsments!");
+    }
+  });
+
+
+
 
 
 
